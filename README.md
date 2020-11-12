721a83b5c0a447a74972b041b489ee9f451bb2e9
brecert <11599528+Brecert@users.noreply.github.com>
2020-11-11 22:11:50 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# 5217
+
+Inspired by https://web.5217.app and the [52/17 work cycle](https://www.themuse.com/advice/the-rule-of-52-and-17-its-random-but-it-ups-your-productivity)
+
+5217 uses three modified icons from [dashicons](https://github.com/WordPress/dashicons) which is licensed under GPL3.
+
+## TODO
+
+- make more expressive, and fun like 5217.app
+- make some of the styling more consistent
+- clean up css/js
---
+++ script.js
@@ -0,0 +1,110 @@
+const visualTimer = document.querySelector(".time");
+const pauseButton = document.querySelector(".pause-play");
+const resetButton = document.querySelector(".reset");
+const themeSelect = document.getElementById("theme-select");
+
+const MINUTE = 60000;
+const WORKTIME = 52;
+const BREAKTIME = 17;
+
+let interval;
+let timeLeft = WORKTIME;
+let timeType = "work";
+
+const intervals = {
+  work: [WORKTIME, 35, 15],
+  break: [BREAKTIME, 10, 5],
+};
+
+const reset = () => {
+  if (interval) {
+    clearInterval(interval);
+    interval = null;
+  }
+  timeType = "work";
+  timeLeft = WORKTIME;
+  pause();
+  update();
+};
+
+const isPaused = () => document.body.classList.contains("paused");
+const pause = () => document.body.classList.add("paused");
+const unpause = () => document.body.classList.remove("paused");
+
+const genTitle = () => {
+  let text = `${timeLeft}m work remaining`;
+  if (isPaused()) {
+    text += ` (paused)`;
+  }
+  return text;
+};
+
+const update = () => {
+  visualTimer.textContent = timeLeft;
+  visualTimer.title = `${timeLeft} minute(s) of ${timeType} remaining`;
+  visualTimer.datetime = `${timeLeft}m`;
+  document.title = genTitle();
+  if (intervals[timeType].includes(timeLeft) && !isPaused()) {
+    new Notification(visualTimer.title);
+  }
+};
+
+const onMinute = () => {
+  timeLeft -= 1;
+  if (timeLeft === 0) {
+    if (timeType === "work") {
+      timeType = "break";
+      timeLeft = BREAKTIME;
+    } else {
+      timeType = "work";
+      timeLeft = WORKTIME;
+    }
+  }
+  update();
+};
+
+let alerted = false;
+const queryNotifications = async () => {
+  if (!("Notification" in window) && !alerted) {
+    alert("This browser does not support desktop notification");
+    alerted = true;
+  } else if (
+    Notification.permission !== "granted" &&
+    Notification.permission !== "denied"
+  ) {
+    await Notification.requestPermission();
+  }
+};
+
+pauseButton.addEventListener("click", async (ev) => {
+  if (interval) {
+    pause();
+    clearInterval(interval);
+    interval = null;
+  } else {
+    unpause();
+    interval = setInterval(onMinute, MINUTE);
+  }
+
+  update();
+});
+
+const updateTheme = () => {
+  switch (themeSelect.value) {
+    case "light":
+      document.body.classList.remove("dark");
+      document.body.classList.add("light");
+      break;
+    case "dark":
+      document.body.classList.remove("light");
+      document.body.classList.add("dark");
+    default:
+      document.body.classList.remove("dark");
+      document.body.classList.remove("light");
+  }
+};
+
+themeSelect.addEventListener("input", updateTheme);
+
+resetButton.addEventListener("click", reset);
+reset();
